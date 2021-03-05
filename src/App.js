c6db3cf8b72ea1c687e4c548c23455cd4dcd3ea6
kento0805-naka <kentz.eva.a.r.xxx@ezweb.ne.jp>
2021-03-05 23:14:01 +0900

    learn props of component

--- src/App.js
+++ src/App.js
@@ -1,32 +1,30 @@
 import React from "react";
 
-// function App() {
-//   return (
-//     <React.Fragment>
-//       <label htmlFor="bar">bar</label>
-
-//       <input
-//         type="text"
-//         onClick={() => {
-//           console.log("I'm clicked!!");
-//         }}
-//       />
-//     </React.Fragment>
-//   );
-// }
-
 const App = () => {
+  const profiles = [
+    { name: "Kento", age: 24 },
+    { name: "Nakamura", age: 64 },
+    { name: "Ito" },
+  ];
   return (
     <div>
-      <Cat />
-      <Cat />
-      <Cat />
-      <Cat />
+      {profiles.map((profile, index) => {
+        return <User name={profile.name} age={profile.age} key={index} />;
+      })}
     </div>
   );
 };
 
-const Cat = () => {
-  return <div>Meow!!</div>;
+const User = (props) => {
+  return (
+    <div>
+      Hi, I am {props.name}, and {props.age} years old!!
+    </div>
+  );
 };
+
+User.defaultProps = {
+  age: 4,
+};
+
 export default App;
